39158de397a7242db074da61a177b3556663fc59
st170001 <st170001@stud.uni-stuttgart.de>
2024-01-10 00:19:38 +0100

    Wishlist comments

--- src/wishlist.rs
+++ src/wishlist.rs
@@ -8,13 +8,20 @@ use bson::datetime::DateTime;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+/// The Wishlist of a user.
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone, SimpleObject)]
 pub struct Wishlist {
+    /// Wishlist UUID.
     pub _id: String,
+    /// UUID of user.
     pub user_id: String,
+    /// UUIDs product variants in Wishlist.
     pub product_variant_ids: HashSet<String>,
+    /// Name of Wishlist.
     pub name: String,
+    /// Timestamp when Wishlist was created. 
     pub created_at: DateTime,
+    /// Timestamp when Wishlist was last updated.
     pub last_updated_at: DateTime,
 }
 
