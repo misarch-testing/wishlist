5deb0326866bd1bdd09cb86f4ea081bd42cfcfda
st170001 <st170001@stud.uni-stuttgart.de>
2024-01-11 17:37:24 +0100

    Remove unneeded serialization helpers

--- src/uuid_serde.rs
+++
@@ -1,9 +0,0 @@
-use bson::Uuid;
-use serde::Serialize;
-
-pub fn serialize_uuid<S>(uuid: &Uuid, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    uuid.serialize(serializer)
-}
