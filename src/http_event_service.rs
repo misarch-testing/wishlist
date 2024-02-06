b764815db73d5aefe17b70b4b676611ae2e51eb0
st170001 <st170001@stud.uni-stuttgart.de>
2024-02-06 11:49:35 +0100

    More comments

--- src/http_event_service.rs
+++ src/http_event_service.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{foreign_types::ProductVariant, user::User};
 
+/// Data to send to Dapr in order to describe a subscription.
 #[derive(Serialize)]
 pub struct Pubsub {
     #[serde(rename(serialize = "pubsubName"))]
@@ -14,34 +15,40 @@ pub struct Pubsub {
     pub route: String,
 }
 
+/// Reponse data to send to Dapr when receiving an event.
 #[derive(Serialize)]
 pub struct TopicEventResponse {
     pub status: i32,
 }
 
+/// Default status is `0` -> Ok, according to Dapr specs.
 impl Default for TopicEventResponse {
     fn default() -> Self {
         Self { status: 0 }
     }
 }
 
+/// Relevant part of Dapr event wrapped in a CloudEnvelope.
 #[derive(Deserialize, Debug)]
 pub struct Event {
     pub topic: String,
     pub data: EventData,
 }
 
+/// Relevant part of Dapr event.data.
 #[derive(Deserialize, Debug)]
 pub struct EventData {
     pub id: Uuid,
 }
 
+/// Service state containing database connections.
 #[derive(Clone)]
 pub struct HttpEventServiceState {
     pub product_variant_collection: Collection<ProductVariant>,
     pub user_collection: Collection<User>,
 }
 
+/// HTTP endpoint to list topic subsciptions.
 pub async fn list_topic_subscriptions() -> Result<Json<Vec<Pubsub>>, StatusCode> {
     let pubsub_user = Pubsub {
         pubsubname: "pubsub".to_string(),
@@ -56,6 +63,8 @@ pub async fn list_topic_subscriptions() -> Result<Json<Vec<Pubsub>>, StatusCode>
     Ok(Json(vec![pubsub_user, pubsub_product_variant]))
 }
 
+
+/// HTTP endpoint to receive events..
 #[debug_handler(state = HttpEventServiceState)]
 pub async fn on_topic_event(
     State(state): State<HttpEventServiceState>,
